7794b6ff89e8807dd9a3db83e921b898191b3a53
Manish Mishra <mk94mishra@gmail.com>
2021-04-08 15:55:36 +0530

    city

--- action/models.py
+++ action/models.py
@@ -1,20 +1,23 @@
 from tortoise.models import Model
-from tortoise.fields import ForeignKeyField, CharField, DatetimeField
+from tortoise.fields import ForeignKeyField, CharField, DatetimeField,IntField
 
 
 class ActionPost(Model):
+    type = CharField(max_length=100)
+
     user = ForeignKeyField('models.User')
     post = ForeignKeyField('models.Post')
-    type = CharField(max_length=100)
     
     description = CharField(max_length=500, null=True)
     media_url = CharField(max_length=500, null=True)
+
+    user_id_blocked = IntField()
 
     created_at = DatetimeField(auto_now_add=True)
 
     class Meta:
         table = "tbl_action"
-        unique_together = (("user", "post","type"), )
+        unique_together = (("user", "post","type"),("user","user_id_blocked") )
 
 
       

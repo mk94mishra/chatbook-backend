530e11ee620fc96d9fd8dbae88d3f3dd59da991b
Manish Mishra <mk94mishra@gmail.com>
2021-05-04 14:45:37 +0530

    action completed

--- atom/action/block/token.py
+++ atom/action/block/token.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from typing import Optional
 from fastapi import APIRouter, Request, status, Depends
 from tortoise.exceptions import DoesNotExist
 
@@ -26,6 +27,33 @@ async def block_create(request: Request, payload: BlockCreate):
     data['updated_by'] = request.state.user_id
     
     return success_response(await Block.create(**data))
+
+# get block 
+@router.get("/block-list", status_code=status.HTTP_200_OK)
+async def user_block_list(request: Request, limit: Optional[int] = 10, offset: Optional[int] = 0):
+    # self user check
+    logged_user_id = int(request.state.user_id)
+    try:
+        async with in_transaction() as connection:
+            sql = """
+            with 
+            ab as (select id, created_at, blocked_id from tbl_block where user_id={logged_user_id})
+
+            select
+            ab.id, ab.blocked_id, ab.created_at ,
+            u.username,u.profile_pic_url
+            from ab
+            left join tbl_user as u on ab.blocked_id=u.id
+            """.format(logged_user_id=logged_user_id)
+
+            orderby = " order by ab.created_at desc limit {limit} offset {offset}".format(limit=limit,offset=offset)
+
+            sql = sql + orderby
+            blocked_user = await connection.execute_query(sql)            
+            return success_response(blocked_user[1])
+    except OperationalError:
+        return error_response(code=400, message="something error!")
+
 
 # delete block 
 @router.delete("/block/{blocked_id}", status_code=status.HTTP_201_CREATED)

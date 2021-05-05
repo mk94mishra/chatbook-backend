683f7f01e39e35e50bf5f6266bb5f3d82ce555bc
Manish Mishra <mk94mishra@gmail.com>
2021-05-05 17:35:23 +0530

    action completed

--- redis.py
+++ redis.py
@@ -18,7 +18,7 @@ def hello_redis():
    
         # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
         # using the default encoding utf-8.  This is client specific.
-        r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+        r = redis.Redis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
    
         # step 4: Set the hello message in Redis
         r.set("msg:hello", "Hello Redis!!!")
